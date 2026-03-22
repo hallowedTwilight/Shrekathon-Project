9187d4723e009120f78b545de3e34e311201dea1
Rev <sunarev@hotmail.com>
2026-03-21 22:48:39 -0400

    Adding scenes
    Added:
    - global script
    - cooking scene
    - main menu scene
    
    + tried to get a start on the whole "health deplete, change phase" thing

--- Components/HealthComponent.cs
+++ Components/HealthComponent.cs
@@ -1,20 +1,37 @@
 using Godot;
 using System;
 
+public Node cookingphase;
+
+public letsgetcooking()
+{
+	cookingphase = ResourceLoader.Load<PackedScene>("res://Scenes/cooking.tscn").Instantiate();
+}
+
+public void _AddASceneManually()
+{
+	// This is like autoloading the scene, only
+	// it happens after already loading the main scene.
+	GetTree().Root.AddChild(cookingphase);
+}
+
 public partial class HealthComponent : Node
 {
-    [Export]
-    public float Health = 10;
-    [Export]
-    public float DrainRate = 1;
-    public override void _Process(double delta)
-    {
-        if(Health > 0)
-        {
-            Health -= DrainRate * (float) delta;
-        } else
-        {
-        }
-    }
+	[Export]
+	public float Health = 10;
+	[Export]
+	public float DrainRate = 1;
+	public override void _Process(double delta)
+	{
+		if(Health > 0)
+		{
+			Health -= DrainRate * (float) delta;
+		} else:
+			//no clue if this actually works lmao
+			_AddASceneManually()
+			SceneTree.change_scene_to_packed(cookingphase)
+		{
+		}
+	}
 
 }
--- Scripts/Player.cs
+++ Scripts/Player.cs
@@ -3,19 +3,19 @@ using System;
 
 public partial class Player : CharacterBody2D
 {
-    [Export]
-    JumpComponent JumpComponent {get; set;}
-    [Export]
-    HealthComponent HealthComponent {get; set;}
-    public override void _Input(InputEvent @event)
-    {
-        if (Input.IsActionJustPressed("game_jump"))
-        {
-            JumpComponent.RequestJump();
-        }
-    }
-    public override void _PhysicsProcess(double delta)
-    {
-        MoveAndSlide();
-    }
+	[Export]
+	JumpComponent JumpComponent {get; set;}
+	[Export]
+	HealthComponent HealthComponent {get; set;}
+	public override void _Input(InputEvent @event)
+	{
+		if (Input.IsActionJustPressed("game_jump"))
+		{
+			JumpComponent.RequestJump();
+		}
+	}
+	public override void _PhysicsProcess(double delta)
+	{
+		MoveAndSlide();
+	}
 }
