e5f11576ca10ccfdeef51cad51d52d111ef02e2d
hallo <hallowedTwilight0000@gmail.com>
2026-03-22 00:39:42 -0400

    add global speed

--- Scripts/Global.cs
+++ Scripts/Global.cs
@@ -3,12 +3,14 @@ using System;
 
 public partial class Global : Node
 {
+	public static Global Instance { get; private set; }
+	[Export]
+	public int Speed {get; set;} = 10;
 	public int JumpCounter = 0;
-	
-	
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		Instance = this;
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
